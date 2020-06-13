f74b022b2d4c7baf991146ad8a143778ad9e54b7
ophielia <ophielia@yahoo.com>
2020-06-13 06:59:11 +0200

    initial commit

---
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit {
+
+  // variable
+  show: boolean;
+
+  constructor(private route: ActivatedRoute,
+    private title: Title,
+    private meta: Meta) {
+  // initialize variable value
+   this.show = false;
+  }
+
+  // click event function toggle
+  password() {
+   this.show = !this.show;
+  }
+
+  ngOnInit() {
+    this.title.setTitle( this.route.snapshot.data['title']);
+  }
+
+}
