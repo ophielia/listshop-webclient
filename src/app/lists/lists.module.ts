10402247a4de1a687c03e5b8fadc8f9c3071503e
ophielia <ophielia@yahoo.com>
2020-09-02 15:22:20 +0200

    LS-416 Web - ongoing search and replace tovo

--- src/app/lists/lists.module.ts
+++ src/app/lists/lists.module.ts
@@ -5,10 +5,12 @@ import { ListsRoutingModule } from './lists-routing.module';
 import { ManageListsComponent } from './manage-lists/manage-lists.component';
 import {ListsComponent} from "./lists.component";
 import {SharedModule} from "../shared/shared.module";
+import {SingleListElementComponent} from "./single-list-element/single-list-element.component";
 
 
 @NgModule({
   declarations: [ManageListsComponent,
+      SingleListElementComponent,
   ListsComponent],
   imports: [
     CommonModule,
