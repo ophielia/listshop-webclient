c7eae1ab925c58116cfdbe4a0c198feee7595554
margaretmartin <ophielia@yahoo.com>
2023-07-09 19:23:05 +0200

    main images in.

--- src/app/beta-campaign/landing/pitch-features/pitch-features.component.ts
+++ src/app/beta-campaign/landing/pitch-features/pitch-features.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pitch-features',
   templateUrl: './pitch-features.component.html',
-  styleUrls: ['./pitch-features.component.css']
+  styleUrls: ['./pitch-features.component.scss']
 })
 export class PitchFeaturesComponent implements OnInit {
 
