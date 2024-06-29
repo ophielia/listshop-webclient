780c47981ca012cd4bebb411d953af15147ff6dc
margaretmartin <ophielia@yahoo.com>
2024-06-29 12:53:54 +0200

    LS-1971 - add ingredient inline.

---
+++ src/app/dishes/add-ingredient-inline-component/add-ingredient-inline.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddIngredientInlineComponent } from './add-ingredient-inline.component';
+
+describe('AddIngredientInlineComponentComponent', () => {
+  let component: AddIngredientInlineComponent;
+  let fixture: ComponentFixture<AddIngredientInlineComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddIngredientInlineComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddIngredientInlineComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/dishes/add-ingredient-inline-component/add-ingredient-inline.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {EditIngredientInlineComponent} from "../edit-ingredient-inline/edit-ingredient-inline.component";
+import {FoodService} from "../../shared/services/food.service";
+import {NGXLogger} from "ngx-logger";
+import {Ingredient} from "../../model/Ingredient";
+import {TagTreeService} from "../../shared/services/tag-tree.service";
+
+@Component({
+  selector: 'app-add-ingredient-inline',
+  templateUrl: './add-ingredient-inline.component.html',
+  styleUrls: ['./add-ingredient-inline.component.scss']
+})
+export class AddIngredientInlineComponent extends EditIngredientInlineComponent {
+
+  constructor(
+       foodService: FoodService,
+       logger: NGXLogger,
+        tagTreeService: TagTreeService
+  ) {
+    super(foodService, logger, tagTreeService);
+    //MM this.editingAmount = false;
+    this.isEditAmount.next(false);
+    this.ingredient = new Ingredient();
+    this.debugTokens = true;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
